c830546d0416479b52f94cdbdf25ce7a6e2d736d
Cristian Gouget <cristiangouget@gmail.com>
2025-03-02 08:31:17 -0300

    Implemented http Basic Security

---
+++ src/main/java/guru/springframework/spring6restmvc/config/SpringSecConfig.java
@@ -0,0 +1,26 @@
+package guru.springframework.spring6restmvc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SpringSecConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeHttpRequests(
+                        authorizationManagerRequestMatcherRegistry -> {
+                    authorizationManagerRequestMatcherRegistry
+                            .anyRequest()
+                            .authenticated();
+                })
+                .httpBasic(Customizer.withDefaults())
+                .csrf((csrf) -> csrf.ignoringRequestMatchers("/api/**"));
+        return httpSecurity.build();
+    }
+}
